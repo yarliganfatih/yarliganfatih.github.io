85af6b215a8fbd363ec874445f089b429f871ff9
Fatih Yarlıgan <fatih.yarligan@bahcesehir.edu.tr>
2023-02-26 19:10:43 +0300

    content filter with footer

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var lastScrolled = [];
 var lastAnimated = [];
 $(".mainParent").scroll(function () {
-    $(".skillSticky").each(function (index, item) {
+    $(".bounceSticky").each(function (index, item) {
         //if(Math.abs(lastScrolled[index] - $(".skillSticky").eq(index).offset().top)<4){
-        if (lastScrolled[index] == $(".skillSticky").eq(index).offset().top) {
+        if (lastScrolled[index] == $(".bounceSticky").eq(index).offset().top) {
             if (!lastAnimated[index]) {
                 $(".colTitle").eq($(this).attr("data-col")).addClass("animateShadowBottom")
                     .on("animationend", function () {
                         $(this).removeClass('animateShadowBottom');
                     });
-                $(".skillSticky").eq(index)
+                $(".bounceSticky").eq(index)
                     .addClass('animateBounce')
                     .on("animationend", function () {
                         $(this).removeClass('animateBounce');
@@ -17,7 +17,7 @@ $(".mainParent").scroll(function () {
                 lastAnimated[index] = 1;
             }
         } else {
-            lastScrolled[index] = $(".skillSticky").eq(index).offset().top;
+            lastScrolled[index] = $(".bounceSticky").eq(index).offset().top;
             lastAnimated[index] = 0;
         }
     });
@@ -45,4 +45,17 @@ $(".nav").on('touchmove', function (e){
     delta = currentY - lastY;
     $(".mainParent").scrollTop($(".mainParent").scrollTop() + delta*-100);
     lastY = currentY;
+});
+
+$(".footer .filterSticky").on('click', function () {
+    var easeHeight = $("body").height() - 300;
+    $(".footer").height(easeHeight);
+    $(this).toggleClass("filterSticky_active");
+    var dataFilter = $(this).attr("data-filter");
+    const easeOut = setTimeout(() => {
+        $("."+dataFilter+"Content").each(function (index, item) {
+            $(this).toggleClass("d-none");
+        });
+        $(".footer").height(60);
+    }, 1001);
 });
